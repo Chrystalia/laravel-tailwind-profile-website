de75adff95e76e110508dc874996b509525fc0b5
Chrystalia <glenysang@gmail.com>
2023-03-29 13:57:43 +0700

    installed autoprefixer & update website profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,26 @@ module.exports = {
     // path/to/template/files
     // "./src/**/*.{html,js}"
     // ex: /resources/views/welcome.blade.php
-    "/resources/**/*.{blade.php, js}"
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'serif': ['Georgia']
+    },
+
+    extend: {
+      animation:{
+        fadeIn: 'fadeIn 1s ease-in'
+      },
+      keyframes: {
+        fadeIn:{
+          '0%': {opacity: 0, transform: 'rotateX(-10deg)'},
+          '100%': {opacity: 1, transform: 'rotateX(0)'},
+        }
+      }
+    },
   },
   plugins: [],
 }
